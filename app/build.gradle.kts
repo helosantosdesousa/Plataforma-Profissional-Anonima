bbde7414d8ee107a6c4edf63d68f5357c5366021
helosantosdesousa <ssheloly@gmail.com>
2025-05-02 15:29:12 -0300

    feito esqueleto tela login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.github.helosantosdesousa.plataformaprofissionalanonima"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.helosantosdesousa.plataformaprofissionalanonima"
